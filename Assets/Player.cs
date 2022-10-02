bb58d36defb047ef26cdac1ac0c1064689e99e72
hktrollgames <hktrollgames@gmail.com>
2022-10-02 14:03:48 +0900

    움직이는 로직 복구

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+    void Update()
+    {
+        UpdateMove();
+    }
+
+    private void UpdateMove()
+    {
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) // z 앞뒤
+            move.z = 1;
+        if (Input.GetKey(KeyCode.S))
+            move.z = -1;
+        if (Input.GetKey(KeyCode.A)) // x 사이드, // 왼쪽.
+            move.x = -1;
+        if (Input.GetKey(KeyCode.D)) // 오른쪽
+            move.x = 1;
+
+        // move 움직임이 있다면 런 애니메이션 해라.
+        // 움직임이 없다면 idle애니메이션 해라.
+        float forward = move.z;
+        float side = move.x;
+        move = move * speed * Time.deltaTime;
+        transform.Translate(move);
+    }
+}
